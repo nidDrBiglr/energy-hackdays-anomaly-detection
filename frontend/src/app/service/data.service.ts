c8742874be95a8b2f297730cd2855c99d02be74a
David Giger <david@akenza.com>
2020-08-28 16:44:44 +0200

    displays json data

--- frontend/src/app/service/data.service.ts
+++ frontend/src/app/service/data.service.ts
@@ -1,19 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class DataService {
+  private url = 'assets/energy.json';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  getData(meterId: string): {}[] {
-    const data = [];
-    let visits = 10;
-    for (let i = 1; i < 366; i++) {
-      visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-      data.push({ date: new Date(2020, 0, i), value: visits });
-    }
-
-    return data;
+  getData(meterId: string): Observable<any> {
+    return this.http.get(this.url);
   }
 
 }
